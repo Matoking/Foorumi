a42dc178934b7b1beabd1e34843bf24a3455136a
Matoking <jannepulk@gmail.com>
2015-12-08 12:06:52 +0200

    Did task 50

--- routes/topics.js
+++ routes/topics.js
@@ -7,34 +7,69 @@ var Models = require('../models');
 // Huom! Kaikki polut alkavat polulla /topics
 
 // GET /topics
-router.get('/', function(req, res, next) {
-    // Hae kaikki aihealueet tässä (Vinkki: findAll)
-    res.send(200);
+router.get('/', function (req, res, next) {
+    Models.Topic.findAll().then(function (topics) {
+        res.send(topics);
+    });
 });
 
 // GET /topics/:id
-router.get('/:id', function(req, res, next) {
-  // Hae aihealue tällä id:llä tässä (Vinkki: findOne)
-  var topicId = req.params.id;
-  res.send(200);
+router.get('/:id', function (req, res, next) {
+    // Hae aihealue tällä id:llä tässä (Vinkki: findOne)
+    var topicId = req.params.id;
+
+    Models.Topic.findOne({where: {id: topicId}}).then(function (topic) {
+        if (topic === null) {
+            res.sendStatus(400);
+            return;
+        }
+        
+        res.send(topic);
+    });
 });
 
 // POST /topics
-router.post('/', function(req, res, next) {
-  // Lisää tämä aihealue
-  var topicToAdd = req.body;
-  // Palauta vastauksena lisätty aihealue
-  res.send(200);
+router.post('/', function (req, res, next) {
+    // Lisää tämä aihealue
+    var topicToAdd = req.body;
+    
+    if (!("name" in topicToAdd) || !("description" in topicToAdd)) {
+        res.sendStatus(400);
+        return;
+    }
+    
+    Models.Topic.create({name: topicToAdd.name,
+                         description: topicToAdd.description}).then(function (topic) {
+        res.send(topic);
+    });
 });
 
 // POST /topics/:id/message
-router.post('/:id/message', function(req, res, next) {
-  // Lisää tällä id:llä varustettuun aihealueeseen...
-  var topicId = req.params.id;
-  // ...tämä viesti (Vinkki: lisää ensin messageToAdd-objektiin kenttä TopicId, jonka arvo on topicId-muuttujan arvo ja käytä sen jälkeen create-funktiota)
-  var messageToAdd = req.body;
-  // Palauta vastauksena lisätty viesti
-  res.send(200);
+router.post('/:id/message', function (req, res, next) {
+    // Lisää tällä id:llä varustettuun aihealueeseen...
+    var topicId = req.params.id;
+    // ...tämä viesti (Vinkki: lisää ensin messageToAdd-objektiin kenttä TopicId, jonka arvo on topicId-muuttujan arvo ja käytä sen jälkeen create-funktiota)
+    var messageToAdd = req.body;
+    
+    if (!("title" in messageToAdd) || !("content" in messageToAdd)) {
+        console.log("Necessary fields missing");
+        res.sendStatus(400);
+        return;
+    }
+    
+    Models.Topic.findOne({where: {id: topicId}}).then(function(topic) {
+        if (topic === null) {
+            console.log("Topic not found");
+            res.sendStatus(400);
+            return;
+        }
+        
+        Models.Message.create({TopicId: topicId,
+                               title: messageToAdd.title,
+                               content: messageToAdd.content}).then(function(message) {
+            res.send(message);
+        });
+    });
 });
 
 module.exports = router;
