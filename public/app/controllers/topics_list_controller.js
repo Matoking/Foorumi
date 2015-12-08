f8c58aef93795223f06109edb3b3f122e099d43a
Matoking <jannepulk@gmail.com>
2015-12-08 12:27:35 +0200

    Did task 51

--- public/app/controllers/topics_list_controller.js
+++ public/app/controllers/topics_list_controller.js
@@ -1,3 +1,13 @@
 FoorumApp.controller('TopicsListController', function($scope, $location, Api){
-  // Toteuta kontrolleri tähän
+    Api.getTopics().success(function(topics) {
+        $scope.topics = topics;
+    });
+    
+    $scope.addTopic = function() {
+        var newTopic = $scope.newTopic;
+        
+        Api.addTopic(newTopic).success(function(topic) {
+            $location.path("/topics/" + newTopic.id);
+        });
+    }
 });
